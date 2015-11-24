16d27a8f7e27a9e6193152b983493c0119cdd633
Tomas Sliz <tomas.sliz@cleverlance.com>
2015-11-24 13:27:05 +0100

    Initial commit

---
+++ SwinjectMVVMExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwinjectMVVMExample
+//
+//  Created by Tomas Sliz on 24/11/15.
+//  Copyright © 2015 Tomas Sliz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
